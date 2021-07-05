a064eaeb8fb6385a872b052b13ee4a1da1978a7f
MyCouples <35689132+MyCouples@users.noreply.github.com>
2021-07-05 17:20:54 +0300

    Конфиг подключения к базе данных

---
+++ php/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$types = array(
+    "Книга",
+    "Статья",
+    "Видео",
+    "Сайт/Блог",
+    "Подборка",
+    "Ключевые идеи книги"
+);
+$db = mysqli_connect('localhost', 'root', 'root', 'materials_makeup') or die('Невозможно подключиться к базе данных materials_makeup');
+
+?>
